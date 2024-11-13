798ba456d45f927eed4effee7e241ef85825294a
Joeri Van Hoof <jvanhoof@fortinet.com>
2024-11-13 11:01:46 +0100

    FMG: TF dev reg update

--- FortiManager/Terraform/single-1nic/terraform/02-fortinet.tf
+++ FortiManager/Terraform/single-1nic/terraform/02-fortinet.tf
@@ -76,7 +76,7 @@ resource "azurerm_network_security_rule" "fmgnsgallowdevregin" {
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
-  destination_port_range      = "514"
+  destination_port_range      = "541"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
 }
